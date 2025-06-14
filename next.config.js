f67088a6222003b79e1a5d06352e7f578533c3e8
hieutongdang <nobjssi0407@gmail.com>
2025-06-14 23:21:25 +0700

    Remove HTTPS redirect and image domain config

--- next.config.js
+++ next.config.js
@@ -3,21 +3,6 @@ const nextConfig = {
   images: {
     domains: ['nextjs.danghieu.com'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/(.*)',
-        has: [
-          {
-            type: 'host',
-            value: 'cuanhua.com.vn',
-          },
-        ],
-        destination: 'https://cuanhua.com.vn/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
-module.exports = nextConfig;
+module.exports = nextConfig; 
